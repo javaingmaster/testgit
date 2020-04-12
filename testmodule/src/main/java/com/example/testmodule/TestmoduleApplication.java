f0a6414e9ee98684f564f757817c08cef2c94b83
javaingmaster <656358805@qq.com>
2020-04-12 09:11:44 +0800

    master commit

---
+++ testmodule/src/main/java/com/example/testmodule/TestmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestmoduleApplication.class, args);
+    }
+
+}
